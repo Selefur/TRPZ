0431e4b3fc83c4050765692c5007bf88ef5e165c
Selefur <145004923+Selefur@users.noreply.github.com>
2024-11-26 21:38:21 +0200

    Added Entitied and Enums

---
+++ TaskManager/DAL/Entities/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaskStatus = DAL.Enums.TaskStatus;
+
+namespace DAL.Entities
+{
+    public class Task
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public required TaskStatus Status { get; set; }
+        public DateTime Deadline { get; set; }
+        public required int UserId { get; set; }
+    }
+}
